e9999ab3315bf94f08a384693b640e9e23e4426a
Miss Darling <TheCanterlonian@users.noreply.github.com>
2024-12-29 23:26:10 -0800

    Update csb.cs

--- csb.cs
+++ csb.cs
@@ -5,23 +5,25 @@ namespace consoleBufferScreenPrinter
 {
 	public class buffer
 	{
-    public static string frame0
-    {}
-    public static string frame1
-    {}
-    public static string frame2
-    {}
-    public static string frame3
-  }
+    		public static string frame0
+    		{}
+    		public static string frame1
+    		{}
+    		public static string frame2
+    		{}
+    		public static string frame3
+		{}
+  	}
 	public class consoler
 	{
 		public static void Main()
 		{
 			//
 		}
-    public static void printBuffer(string)
-    {
-      Console.Write(string);
-    }
+    		public static void printBuffer(string)
+    		{
+      			Console.Write(string);
+		}
 	}
 }
+
