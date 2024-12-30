a18fa78048cd491744e5ecdd84791d9474549b3d
Miss Darling <TheCanterlonian@users.noreply.github.com>
2024-12-29 23:24:09 -0800

    Create csb.cs

---
+++ csb.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+
+namespace consoleBufferScreenPrinter
+{
+	public class buffer
+	{
+    public static string frame0
+    {}
+    public static string frame1
+    {}
+    public static string frame2
+    {}
+    public static string frame3
+  }
+	public class consoler
+	{
+		public static void Main()
+		{
+			//
+		}
+    public static void printBuffer(string)
+    {
+      Console.Write(string);
+    }
+	}
+}
